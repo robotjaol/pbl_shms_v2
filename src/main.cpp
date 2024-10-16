9aaa5d8f0d2730c363d5c5347c3fae1e17fe8eb9
= <jonathanoktavianofrizzy@gmail.com>
2024-10-16 09:48:50 +0700

    setup library

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <FreeRTOS.h>
+#include <Adafruit_ADXL345_U.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_SSD1306.h>
+#include <HX711.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+
+#define RESET_MPU 2
+#define RESET_ADXL345 3
+#define RESET_OLED 4
+
+void setup()
+{
+}
+void loop()
+{
+}
